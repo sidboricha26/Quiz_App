7e916f7f111f23ee367be6c93aac773b0814b414
sidboricha26 <83353666+sidboricha26@users.noreply.github.com>
2023-08-31 16:02:45 +0530

    final commit

--- quiz_main.py
+++ quiz_main.py
@@ -1,38 +1,22 @@
+import json
 import tkinter
 from tkinter import *
 import random
 
-questions = [
-    "This is sample question 1 ?",
-    "This is sample question 2 ?",
-    "This is sample question 3 ?",
-    "This is sample question 4 ?",
-    "This is sample question 5 ?",
-    "This is sample question 6 ?",
-    "This is sample question 7 ?",
-    "This is sample question 8 ?",
-    "This is sample question 9 ?",
-    "This is sample question 10 ?",
-]
-
-answers_choice = [
-    ["1", "2", "3", "4",],
-    ["1", "2", "3", "4",],
-    ["1", "2", "3", "4",],
-    ["1", "2", "3", "4",],
-    ["1", "2", "3", "4",],
-    ["1", "2", "3", "4",],
-    ["1", "2", "3", "4",],
-    ["1", "2", "3", "4",],
-    ["1", "2", "3", "4",],
-    ["1", "2", "3", "4",],
-]
-
-answers = [1,2,3,0,2,3,1,0,3,2]
+answers = [1,1,1,1,3,1,0,1,3,3]
 
 user_answer = []
 
+# load questions and answer choices from json file instead of the file
+with open('./data.json', encoding="utf8") as f:
+    data = json.load(f)
+
+# convert the dictionary in lists of questions and answers_choice
+questions = [v for v in data[0].values()]
+answers_choice = [v for v in data[1].values()]
+
 indexes = []
+#function that randomly select 5 questions
 def gen():
     global indexes
     while(len(indexes) < 5):
@@ -42,38 +26,60 @@ def gen():
         else:
             indexes.append(x)
 
+#function to show result based on the answer selected by user
 def show_result(score):
     lbl_Question.destroy()
     r1.destroy()
     r2.destroy()
     r3.destroy()
     r4.destroy()
+
+    #result image
     label_image = Label(
         root,
+        bg= "white",
+        border= 0,
     )
-    label_image.pack()
+    label_image.pack(pady=(50, 30))
+
+    #result text
     label_resultxt = Label(
         root,
         font= ("Consolas", 20),
+        bg= "white",
     )
     label_resultxt.pack()
+
     if score >= 20:
         img = PhotoImage(file="Images/great.png")
         label_image.configure(image=img)
+        label_image.image = img
         label_resultxt.configure(text="You are Excellent !!")
+    elif (score >= 10 and score < 20):
+        img = PhotoImage(file="Images/ok.png")
+        label_image.configure(image=img)
+        label_image.image = img
+        label_resultxt.configure(text="You can be better")
+    else:
+        img = PhotoImage(file="Images/bad.png")
+        label_image.configure(image=img)
+        label_image.image = img
+        label_resultxt.configure(text="You should work hard")
 
+#function to calculate score
 def calc():
     global indexes, user_answer, answers
     x = 0
     score = 0
     for i in indexes:
-        if user_answer == answers[i]:
+        if user_answer[x] == answers[i]:
             score = score + 5
         x += 1
     show_result(score)
 
 
 ques = 1
+#function to select a option
 def selected():
     global radio_var, user_answer
     global lbl_Question, r1, r2, r3, r4
@@ -90,12 +96,14 @@ def selected():
     else:
         calc()
 
+#Question
 def start_quiz():
+
     global lbl_Question, r1, r2, r3, r4
     lbl_Question = Label(
         root,
         text=questions[indexes[0]],
-        font= ("Consolas", 16),
+        font= ("Ebrima Bold", 16),
         width= 500,
         justify= "center",
         wraplength= 400,
@@ -103,14 +111,16 @@ def start_quiz():
     )
     lbl_Question.pack(pady=(100, 30))
 
+
     global radio_var
     radio_var = IntVar()
     radio_var.set(-1)
 
+    # 4 Options button
     r1 = Radiobutton(
         root,
         text=answers_choice[indexes[0]][0],
-        font= ("Times", 12),
+        font= ("Ebrima Bold", 12),
         value= 0,
         variable= radio_var,
         bg="white",
@@ -121,7 +131,7 @@ def start_quiz():
     r2 = Radiobutton(
         root,
         text=answers_choice[indexes[0]][1],
-        font=("Times", 12),
+        font=("Ebrima Bold", 12),
         value=1,
         variable=radio_var,
         bg="white",
@@ -132,7 +142,7 @@ def start_quiz():
     r3 = Radiobutton(
         root,
         text=answers_choice[indexes[0]][2],
-        font=("Times", 12),
+        font=("Ebrima Bold", 12),
         value=2,
         variable=radio_var,
         bg="white",
@@ -143,7 +153,7 @@ def start_quiz():
     r4 = Radiobutton(
         root,
         text=answers_choice[indexes[0]][3],
-        font=("Times", 12),
+        font=("Ebrima Bold", 12),
         value=3,
         variable=radio_var,
         bg="white",
@@ -151,6 +161,7 @@ def start_quiz():
     )
     r4.pack(pady=5)
 
+#Start button function
 def start_pressed():
     label_img.destroy()
     label_text.destroy()
@@ -166,6 +177,7 @@ root.title("Quiz Game")
 root.geometry("700x600")
 root.resizable(False, False)
 
+#Main Quiz image
 img1 = PhotoImage(file="Images/main-icon-2.png")
 
 label_img = Label(
@@ -174,6 +186,7 @@ label_img = Label(
 )
 label_img.pack(pady=(40,0))
 
+#Quiz Time label
 label_text = Label(
     root,
     text="Quiz Time",
@@ -181,6 +194,7 @@ label_text = Label(
 )
 label_text.pack(pady=(0, 30))
 
+#Start butto image
 img2 = PhotoImage(file="Images/start-1.png")
 
 btn_Start = Button(
@@ -192,6 +206,7 @@ btn_Start = Button(
 )
 btn_Start.pack(pady=(0, 10))
 
+#instruction about quiz
 lbl_Instruct = Label(
     root,
     text="Read the Rules and\n Click Start once you are ready",
@@ -199,12 +214,12 @@ lbl_Instruct = Label(
     font= ("Consolas", 14),
     justify= "center",
 )
-lbl_Instruct.pack(pady=(10, 70))
+lbl_Instruct.pack(pady=(10, 90))
 
+#Rules about quiz
 lbl_Rules = Label(
     root,
-    text="This quiz contain 10 questions\n "
-         "You will get 20 seconds to solve a questions\n "
+    text="This quiz contain 5 questions\n "
          "Once you select an option that will be a final choice\n "
          "Hence, think before you select",
     width= 100,
